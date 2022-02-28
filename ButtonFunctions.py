3798ae4b19717155b685e446a03b90111a8e1f9e
adragon9 <39833575+adragon9@users.noreply.github.com>
2022-02-28 08:11:59 -0600

    Added the file writing functionality

--- ButtonFunctions.py
+++ ButtonFunctions.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from os.path import exists
 
 import Windows
 from Calculators import correction_calculator
@@ -17,18 +18,29 @@ def submit_bs():
 
     # No reason to read anything higher than 3 digits as that would mean you are in the hospital
     # Gives confirmation of success and lets the user know if the input is wrong
-    warningLabel = Label(Windows.Window1.root)
-    if 3 >= len(Windows.Window1.bsEntry.get()) > 0:
+    bsEntry = Windows.Window1.bsEntry
+    root = Windows.Window1.root
+    warningLabel = Label(root)
+
+    if 3 >= len(bsEntry.get()) > 0:
         warningLabel['foreground'] = "green"
         warningLabel['text'] = "Submitted"
         warningLabel.grid(row=1, column=2)
         warningLabel.after(5000, warningLabel.destroy)
-        print(Windows.Window1.bsEntry.get())  # debug print used until file writing was set up
-        correction_calculator(int(Windows.Window1.bsEntry.get()))
+        print(bsEntry.get())  # debug print used until file writing was set up
+        correction_calculator(int(bsEntry.get()))
     else:
         warningLabel['foreground'] = "red"
         warningLabel['text'] = "Entry Invalid"
         warningLabel.grid(row=1, column=2)
         warningLabel.after(5000, warningLabel.destroy)
-        Windows.Window1.bsEntry.delete(0, len(Windows.Window1.bsEntry.get()))
+        bsEntry.delete(0, len(bsEntry.get()))
         print("Value out of range")
+
+    fileName = "Test.txt"
+    if not exists("Test.txt"):
+        with open(fileName, 'w') as logFile:
+            logFile.write("TEST\n{}\n".format(bsEntry.get()))
+    else:
+        with open(fileName, 'a') as logFile:
+            logFile.write("{}\n".format(bsEntry.get()))
