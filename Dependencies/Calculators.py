59024c5c1832c064f9f32947c882c82416936c99
adragon9 <39833575+adragon9@users.noreply.github.com>
2022-03-07 19:40:28 -0600

    Rewrote a lot of the code, hopefully it's prepared for implementing a second window now.

--- Dependencies/Calculators.py
+++ Dependencies/Calculators.py
@@ -1,9 +1,11 @@
-def correction_calculator(blood_sugar):
+def correction_calculator(blood_sugar, target=None, correction_factor=None):
 
     correction = (blood_sugar - 120) / 20  # formula for the calculator personal to each person have to change
 
 # if the blood sugar is higher than target print correction else print no correction
     if blood_sugar <= 120:
-        print("No Correction")
+        print("No correction")
+        return "No correction"
     else:
         print("Your correction is -->", correction)
+        return str(correction)
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from tkinter import mainloop
+from Dependencies.appWindowConfig import MainWindow
 
-from Dependencies import Windows
 
 # Main script
 if __name__ == "__main__":
-    Windows.window_1_layout()  # calls the window formatting function
-mainloop()
+    MainWindow.root.mainloop()
